2b9c57dbaf93a3c02c5d106d03c335dd1bd5ed5c
Sergey Zaborovsky <seryiza@yandex.ru>
2021-05-25 23:15:07 +1100

    Добавлен первый день по Prolog

---
+++ Prolog/day1_books.pl
@@ -0,0 +1,5 @@
+book('The Call of Cthulhu', 'H. P. Lovecraft').
+book('The Book', 'H. P. Lovecraft').
+book('Prolog Programming for Artificial Intelligence', 'I. Bratko').
+
+% book(What, 'H. P. Lovecraft').
---
+++ Prolog/day1_music.pl
@@ -0,0 +1,11 @@
+instrument('Joe', 'guitar').
+instrument('Kate', 'guitar').
+instrument('Andrew', 'piano').
+instrument('Sergey', 'viola').
+
+genre('Joe', 'rock').
+genre('Kate', 'pop').
+genre('Andrew', 'baroque').
+genre('Sergey', 'pop').
+
+% instrument(X, 'guitar').
