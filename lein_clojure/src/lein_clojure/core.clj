49e6005e100194cd69d0d8a523994f4fa3fadd96
Sergey Zaborovsky <seryiza@yandex.ru>
2021-06-28 18:32:13 +1100

    Добавлен второй день по Clojure

--- lein_clojure/src/lein_clojure/core.clj
+++ lein_clojure/src/lein_clojure/core.clj
@@ -7,3 +7,14 @@
   (if (= (type coll) clojure.lang.PersistentList) :list
   (if (= (type coll) clojure.lang.PersistentVector) :vector
   (if (= (type coll) clojure.lang.PersistentArrayMap) :map))))
+
+;; Day 2
+(defmacro unless [test body else]
+  (list 'if (list 'not test) body else))
+
+(defprotocol MyPrinter
+  (print-all [this]))
+
+(defrecord CollPrinter [coll]
+  MyPrinter
+  (print-all [_] (map println coll)))
